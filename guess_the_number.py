f1590f59ec5d0c2bc0ccab5dad1a9b7930f005c1
RanjeetKumbhar01 <90677720+RanjeetKumbhar01@users.noreply.github.com>
2022-07-07 18:45:14 +0530

    Create guess_the_number.py

---
+++ guess_the_number.py
@@ -0,0 +1,24 @@
+import random
+
+randno = random.randint(1, 5)
+
+guess = 0
+userguess = None
+while (userguess != randno):
+    userguess = int(input("Enter your guess: "))
+    guess += 1
+    if userguess == randno:
+        print("You got it!!!!")
+    else:
+        if userguess < randno:
+            print("You guessed wrong!! Enter larger number ")
+        else:
+            print("You guessed wrong!! Enter smaller number ")
+
+print(f"You guess the number in {guess} guesses")
+with open("highscore.txt", "r") as f:
+    hiscore = int(f.read())
+if (hiscore < guess):
+    print("You broken the highest score!!!")
+    with open("highscore.txt", "w") as f:
+        f.write(str(guess))
